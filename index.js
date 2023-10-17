3d5624295ab93f125f1728ac017070948f3578a4
Chan Ee Fang <chaneefang@gmail.com>
2023-10-18 01:20:29 +0800

    Wrong area of declaration for socket.io

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express=require("express")
-const app=express()
+const express=require("express");
+const app=express();
 
 // declare required dependencies  
 const path=require("path")
-const http=require("http")
-const {Server}=require("socket.io")
+const http=require("http");
+const {Server}=require("socket.io");
 
 
-const server=http.createServer(app) //create server in HTTP
+const server=http.createServer(app); //create server in HTTP
 
-const io=new Server(server)
+
+const io=new Server(server);
+
 app.use(express.static(path.resolve(""))) // to successfully use app constant, indicate path of HTML file
 
 let arr=[]
@@ -50,9 +52,10 @@ io.on("connection",(socket)=>{ // to get the name of the user
 })
 
 app.get("/",(req,res)=>{
-  return res.sendFile("index.html")
-})
+  return res.sendFile("index.html");
+});
 
 server.listen(3000,()=>{
-  console.log("port connected to 3000")
-})
+  console.log("port connected to 3000");
+});
+
